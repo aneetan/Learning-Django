df864a6844168fad21596c97dea60bc6dad4c0fe
aneetan <anitaneupane2061@gmail.com>
2024-07-27 11:45:25 +0545

    Django Template engines

--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
+
+rooms = [
+    {'id' : 1, 'name' : "Python Django"},
+    {'id' : 2, 'name' : "UI/UX Design"},
+    {'id' : 3, 'name' : "Frontend Development"}
+
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html', {'r': rooms})
 
 def room(request):
     return render(request, 'room.html')
