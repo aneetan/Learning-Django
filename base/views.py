0bb3fae9a553cd44d59de3ff9e27fa00c615b378
aneetan <anitaneupane2061@gmail.com>
2024-07-27 12:54:27 +0545

    Operations in model

--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
 
 
-rooms = [
-    {'id' : 1, 'name' : "Python Django"},
-    {'id' : 2, 'name' : "UI/UX Design"},
-    {'id' : 3, 'name' : "Frontend Development"}
+# rooms = [
+#     {'id' : 1, 'name' : "Python Django"},
+#     {'id' : 2, 'name' : "UI/UX Design"},
+#     {'id' : 3, 'name' : "Frontend Development"}
 
-]
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'r': rooms})
 
 def room(request, pk):  
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     return render(request, 'room.html', {'room': room})
