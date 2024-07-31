0b34d6adb9cea8b7aca5a060a59568c06ce29df5
aneetan <anitaneupane2061@gmail.com>
2024-07-31 23:17:36 +0545

    User registration completed

--- base/views.py
+++ base/views.py
@@ -4,15 +4,21 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     #getting values from frontend
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -31,8 +37,32 @@ def loginPage(request):
             messages.error(request, 'Incorrect username or password')
 
 
-    context = {} 
+    context = {'page':page} 
     return render(request, 'base/login_register.html', context)
+
+def registerUser(request):
+    page = 'register'
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            #commit False to get that user instantly
+            user = form.save(commit=False)
+
+            #clean data
+            user.username = user.username.lower()
+            user.save()
+
+            #send the user to home page with loeed in status
+            login(request, user)
+            return redirect('home')
+        
+        else:
+            messages.error(request, 'Error occured')
+
+    return render(request, 'base\login_register.html', {'form': form })
 
 
 def home(request):
