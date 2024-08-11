22e680237487eda4dba13d7360dc790cb204951e
aneetan <anitaneupane2061@gmail.com>
2024-08-11 10:33:40 +0545

    Migrating user model

---
+++ usermodel/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+#user model having all original functionality of a user model
+class User(AbstractUser):
+    pass
